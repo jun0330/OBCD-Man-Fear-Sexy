7d6868af585667708ff748b6896496d313c7f843
Administrator <Administrator@stu17003>
2018-06-05 09:15:50 +0800

    ADDRESS实体类

---
+++ src/com/etc/entity/Address.java
@@ -0,0 +1,65 @@
+package com.etc.entity;
+
+public class Address {
+	private int ADDRESSID;
+	private String ADDRESSNAME;
+	private String ADDRESSPHONE;
+	private String ADDRESSADDR;
+	private int USERID;
+	
+	
+	
+	public Address(String aDDRESSNAME, String aDDRESSPHONE, String aDDRESSADDR, int uSERID) {
+		super();
+		ADDRESSNAME = aDDRESSNAME;
+		ADDRESSPHONE = aDDRESSPHONE;
+		ADDRESSADDR = aDDRESSADDR;
+		USERID = uSERID;
+	}
+	public Address(int aDDRESSID, String aDDRESSNAME, String aDDRESSPHONE, String aDDRESSADDR, int uSERID) {
+		super();
+		ADDRESSID = aDDRESSID;
+		ADDRESSNAME = aDDRESSNAME;
+		ADDRESSPHONE = aDDRESSPHONE;
+		ADDRESSADDR = aDDRESSADDR;
+		USERID = uSERID;
+	}
+	public int getADDRESSID() {
+		return ADDRESSID;
+	}
+	public void setADDRESSID(int aDDRESSID) {
+		ADDRESSID = aDDRESSID;
+	}
+	public String getADDRESSNAME() {
+		return ADDRESSNAME;
+	}
+	public void setADDRESSNAME(String aDDRESSNAME) {
+		ADDRESSNAME = aDDRESSNAME;
+	}
+	public String getADDRESSPHONE() {
+		return ADDRESSPHONE;
+	}
+	public void setADDRESSPHONE(String aDDRESSPHONE) {
+		ADDRESSPHONE = aDDRESSPHONE;
+	}
+	public String getADDRESSADDR() {
+		return ADDRESSADDR;
+	}
+	public void setADDRESSADDR(String aDDRESSADDR) {
+		ADDRESSADDR = aDDRESSADDR;
+	}
+	public int getUSERID() {
+		return USERID;
+	}
+	public void setUSERID(int uSERID) {
+		USERID = uSERID;
+	}
+	@Override
+	public String toString() {
+		return "Address [ADDRESSID=" + ADDRESSID + ", ADDRESSNAME=" + ADDRESSNAME + ", ADDRESSPHONE=" + ADDRESSPHONE
+				+ ", ADDRESSADDR=" + ADDRESSADDR + ", USERID=" + USERID + "]";
+	}
+	
+	
+
+}
